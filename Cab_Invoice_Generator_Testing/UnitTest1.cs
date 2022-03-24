e34d9ed730c7f5c37f6d6c1292d5b09324c2d44f
unknown <chilamakooru.gangaiiit@gmail.com>
2022-03-24 11:47:52 +0530

    Calculating Fair for Single ride

--- Cab_Invoice_Generator_Testing/UnitTest1.cs
+++ Cab_Invoice_Generator_Testing/UnitTest1.cs
@@ -1,18 +1,29 @@
+using Cab_Invoice_Program;
 using NUnit.Framework;
 
 namespace Cab_Invoice_Generator_Testing
 {
     public class Tests
     {
+        public CabInvoiceGenerator cabInvoiceGenerator;
+        //cost per km = 10 & per min = 1
         [SetUp]
         public void Setup()
         {
+            cabInvoiceGenerator = new CabInvoiceGenerator();
         }
-
+        //Given distance in kmand time in minshould generate fair
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            double Fair = cabInvoiceGenerator.CalcuteFair(2,5);
+            Assert.AreEqual(25, Fair);
+        }
+        [Test]
+        public void Test2()
+        {
+            double Fair = cabInvoiceGenerator.CalcuteFair(0,0);
+            Assert.AreEqual(5, Fair);
         }
     }
 }
---
+++ Cab_Invoice_Program/CabInvoiceGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cab_Invoice_Program
+{
+    public class CabInvoiceGenerator
+    {
+        const int COST_PER_KM = 10;
+        const int COST_PER_MINUTE = 1;
+        const int MINIMUM_FARE = 5;
+        public double CalcuteFair(double distance, double time)
+        {
+            double Fare = distance * COST_PER_KM + time * COST_PER_MINUTE;
+            if (Fare > MINIMUM_FARE)
+            {
+                return Fare;
+            }
+            return MINIMUM_FARE;
+        }
+
+        
+    }
+}
