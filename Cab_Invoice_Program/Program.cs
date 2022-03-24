ce613bf70136547b498ab863f8c9d0ea8ad406f7
unknown <chilamakooru.gangaiiit@gmail.com>
2022-03-24 10:43:24 +0530

    Cab invoice Program

---
+++ Cab_Invoice_Program/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Cab_Invoice
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator program.");
+
+        }
+    }
+}
+
